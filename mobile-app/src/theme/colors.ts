842d4f8e72f2189ae7120da2e3749860057e415e
Rebecca Marusca <maruscarebecca@gmail.com>
2026-08-19 16:09:25 +0300

    changed wave header logic to include ability to change its colors for future use fixed small forgot password sheet bug removed old unused colors and added new ones

--- mobile-app/src/theme/colors.ts
+++ mobile-app/src/theme/colors.ts
@@ -1,16 +1,14 @@
 export const colors = {
-  lightGreen: '#e6efe9',
-  darkGreen: '#557473',
-  blue: '#596488',
-  shadowGrey: '#352E38',
   white: '#ffffff',
-  lightBlue: '#C3CCD1',
   base: '#E7EFEA',
   bgSecondary1: '#DDE7DF',
   bgSecondary2: '#CDD9D5',
   text: '#352D38',
   textMuted: '#4B696E',
   accent: '#3E7466',
+  greenWave3: "#386A5A",
+  greenWave2: "#2D5A4C",
+  greenWave1: "#1E4438"
 };
 
 export type ColorToken = keyof typeof colors;
