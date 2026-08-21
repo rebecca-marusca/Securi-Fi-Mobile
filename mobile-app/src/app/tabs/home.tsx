26b4a8ed24e1705cef7a306ab59fa10b8573711c
Rebecca Marusca <maruscarebecca@gmail.com>
2026-08-21 14:24:18 +0300

    intre miezu noptii si 5 dimineata iti zice hello:))

--- mobile-app/src/app/tabs/home.tsx
+++ mobile-app/src/app/tabs/home.tsx
@@ -13,6 +13,7 @@ import { useAuth } from '@/contexts/AuthContext';
 
 function getGreeting(): string {
   const hour = new Date().getHours();
+  if (hour <= 5) return "Hello";
   if (hour > 5 && hour < 12) return "Good morning";
   if (hour < 18) return "Good afternoon";
   return "Good evening";
